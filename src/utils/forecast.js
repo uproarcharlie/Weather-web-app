23701a018ad6c0d7846e4177ff3890621f7ed3da
Charlie Meaden <charlie@uproar.gg>
2019-07-04 21:53:23 +1200

    adding max temp to forecast and info in the about page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ request({url, json: true}, (error, { body }) => {
     } else if (body.error) {
         callback('unable to find location', undefined)
     } else {
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% change of rain.')
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% change of rain with highs of ' + body.daily.data[0].temperatureMax + 'C.' )
 }
 })
 }
